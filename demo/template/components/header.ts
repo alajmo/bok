c7def550b0f832652503d7d996b34f9ba364d918
samiralajmovic <alajmovic.samir@gmail.com>
2020-04-02 13:51:31 +0200

    wip

--- demo/template/components/header.ts
+++ demo/template/components/header.ts
@@ -11,13 +11,13 @@ export default function(site) {
         <nav>
           <ul>
             <li>
-              <a href="/posts">~/Posts</a>
+              <a href="/posts">Posts</a>
             </li>
             <li>
-              <a href="/projects">~/Projects</a>
+              <a href="/projects">Projects</a>
             </li>
             <li>
-              <a href="/about">~/About</a>
+              <a href="/about">About</a>
             </li>
           </ul>
         </nav>
